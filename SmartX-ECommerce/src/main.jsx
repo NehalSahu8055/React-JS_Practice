1191e0d46a131d53b64fc8bbc72cfb313e97bf14
Nehal Sahu <ProfessionalNehal@outlook.com>
2024-01-07 18:47:30 +0530

    Add Forms Component

--- SmartX-ECommerce/src/main.jsx
+++ SmartX-ECommerce/src/main.jsx
@@ -3,18 +3,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import '../stylesheets/reset.css'
 import '../stylesheets/index.css'
+import '../stylesheets/forms.css';
 import { Provider } from 'react-redux'
 import { store } from './app/store.js'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import RootLayout from './layout/RootLayout';
 import Home from './pages/Home.jsx'
 import Cart from './pages/Cart.jsx'
+import AddAddress from './pages/AddAddress.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<RootLayout />}>
       <Route path='' element={<Home />} />
-      <Route path='/cart' element={<Cart />} />
+      <Route path='/cart/add-address' element={<AddAddress />} >
+        {/* <Route path='/cart' element={<Cart />} > */}
+
+      </Route>
+
     </Route>
   )
 );
---
+++ SmartX-ECommerce/src/pages/AddAddress.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AddressForm from "../components/forms/AddressForm/AddressForm"
+
+function AddAddress() {
+    return (
+        <AddressForm />
+    )
+}
+
+export default AddAddress
